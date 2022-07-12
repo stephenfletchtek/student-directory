cf1a6bdf845cb31e8a30b618f0aa91a7b54fb40d
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-07-12 22:08:26 +0100

    question 7

--- directory2.rb
+++ directory2.rb
@@ -1,18 +1,18 @@
 # let's put all students into an array
 # hobby, country, height
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november, hobby: :chess, country: :UK, height: "2m"},
-{name: "Darth Vader", cohort: :november, hobby: "", country: "", height: ""},
-{name: "Nurse Ratched", cohort: :november, hobby: "", country: "", height: ""},
-{name: "Michael Corleone", cohort: :november, hobby: "", country: "", height: ""},
-{name: "Alex DeLarge", cohort: :november, hobby: "", country: "", height: ""},
-{name: "The Wicked Witch of the West", cohort: :november, hobby: "", country: "", height: ""},
-{name: "Terminator", cohort: :november, hobby: "", country: "", height: ""},
-{name: "Freddy Krueger", cohort: :november, hobby: "", country: "", height: ""},
-{name: "The Joker", cohort: :november, hobby: "", country: "", height: ""},
-{name: "Joffrey Baratheon", cohort: :november, hobby: "", country: "", height: ""},
-{name: "Norman Bates", cohort: :november, hobby: "", country: "", height: ""}
-]
+# students = [
+# {name: "Dr. Hannibal Lecter", cohort: :november, hobby: :chess, country: :UK, height: "2m"},
+# {name: "Darth Vader", cohort: :november, hobby: "", country: "", height: ""},
+# {name: "Nurse Ratched", cohort: :november, hobby: "", country: "", height: ""},
+# {name: "Michael Corleone", cohort: :november, hobby: "", country: "", height: ""},
+# {name: "Alex DeLarge", cohort: :november, hobby: "", country: "", height: ""},
+# {name: "The Wicked Witch of the West", cohort: :november, hobby: "", country: "", height: ""},
+# {name: "Terminator", cohort: :november, hobby: "", country: "", height: ""},
+# {name: "Freddy Krueger", cohort: :november, hobby: "", country: "", height: ""},
+# {name: "The Joker", cohort: :november, hobby: "", country: "", height: ""},
+# {name: "Joffrey Baratheon", cohort: :november, hobby: "", country: "", height: ""},
+# {name: "Norman Bates", cohort: :november, hobby: "", country: "", height: ""}
+# ]
 
 def print_header
   puts "The students of Villains Academy"
@@ -39,17 +39,12 @@ def print(names, search="")
       record = names[index]
       puts "#{index + 1}. #{record[:name]} (#{record[:cohort]} cohort)"
       str = "Hobby: #{record[:hobby]} Country: #{record[:country]} Height: #{record[:height]}"
-      # account for inxed number to have varying length
+      # account for index number to have varying length
       num = (index + 1).to_s.length.to_i * 2 + 4
       # align hobbies under first line 
       puts str.center(str.length + num)
       index += 1
     end
-
-    # names.each {} technique
-    # names.each_with_index do |name, index|
-    #   puts "#{index + 1}. #{name[:name]} (#{name[:cohort]} cohort)"
-    # end
   end
 end
 
@@ -58,19 +53,35 @@ def print_footer(names)
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
+  # modified for question 7
   students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add student hash to the array
-    students << {name: name, cohort: :november, hobby: :chess, country: :UK, height: "2m"}
+  categories = [:name, :cohort, :hobby, :country, :height]
+  while true
+    student = {}
+    while true
+      categories.each do |category|
+        puts "Enter student's #{category}"
+        input = gets.chomp
+        input = :blank if input == "" 
+        if category == :name || category == :height
+          student[category] = input
+        else
+          student[category] = input.to_sym
+        end
+      end
+      puts student
+      puts "Accept details? [Y/n]"
+      input = gets.chomp
+      break if input.upcase != "N" 
+
+    end
+    puts "Student details accepted"
+    students << student
     puts "Now we have #{students.count} students"
-    # get another name from the user
-    name = gets.chomp
+    puts "Add another student? [Y/n]"
+    input = gets.chomp
+    break if input.upcase == "N"
+
   end
   # return array of students
   students
@@ -83,7 +94,7 @@ def do_filter
   return gets.chomp
 end
 
-# students = input_students
+students = input_students
 print_header
 print(students, do_filter)
 print_footer(students)
